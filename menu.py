bea975a55046c3f598fbf0063326bb4832d83838
Nathan Gardner <brytstahr@yahoo.com>
2018-04-22 05:56:26 -0700

    added settings dropdown menu

--- menu.py
+++ menu.py
@@ -78,7 +78,7 @@ class Ripple(Page):
         Page.__init__(self, *args, **kwargs)
         label = tk.Label(self, text="Current Ripple Price")
         label.pack(side="top", pady=50)
-        button = tk.Button(self, text="Graph (1 Month)", bg="purple", fg="white", width=25, height=4,
+        button = tk.Button(self, text="Graph (1 Month)", bg="purple", fg="white", width=25, height=5,
                            command=lambda: graph.graph(dict))
         button.pack(side="left", padx=130, pady=50)
         button = tk.Button(self, text="Graph (1 Week)", bg="purple", fg="white", width=25, height=4,
@@ -110,10 +110,13 @@ class Home(Page):
         label = tk.Label(self, text="Click any of the buttons to begin")
         label.pack(side="bottom", fill="both", expand=True)
 
-#class Settings(Page):
-    #def __init__(self, *args, **kwargs):
-        #Page.__init__(self, *args, **kwargs)
-        #userInput = Entry(self)
+class Settings(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        userInput = tk.Entry(self)
+        userInput.pack(side="right")
+        label = tk.Label(self, text="Phone number")
+        label.pack(side="right")
         #add text input for phone number
 
 class MainView(tk.Frame):
@@ -125,14 +128,17 @@ class MainView(tk.Frame):
         p4 = Ripple(self)
         p5 = Eosio(self)
         p6 = Home(self)
+        p7 = Settings(self)
 
         #create frames for buttons
         buttonframe = tk.Frame(self)
         container = tk.Frame(self)
         container2 = tk.Frame(self)
+        settingsContainer = tk.Frame(self)
         buttonframe.pack(side="top", fill="x", padx = 10, pady=10, expand=False)
         container.pack(side="top", fill="both", expand=True)
         container2.pack(side="bottom", pady=10)
+        settingsContainer.pack(side="right", pady=100)
 
         #place the name of the button page in the middle of screen
         p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
@@ -140,7 +146,8 @@ class MainView(tk.Frame):
         p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         p4.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         p5.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p6.place(in_=container, x=0, y=1, relwidth=1, relheight=1)
+        p6.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p7.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
         #button properties
         b1 = tk.Button(buttonframe, text="Bitcoin", bg="orange", fg="white", width = 20, height = 5, command=p1.lift)
@@ -149,6 +156,7 @@ class MainView(tk.Frame):
         b4 = tk.Button(buttonframe, text="Ripple", bg="blue", fg="white", width = 20, height = 5, command=p4.lift)
         b5 = tk.Button(buttonframe, text="Eosio", bg="black", fg="white", width = 20, height = 5, command=p5.lift)
         b6 = tk.Button(container2, text="Menu", bg="gray", fg="white", width = 20, height = 5, command=p6.lift)
+        #b7 = tk.Button(settingsContainer, text="Settings", bg="gray", fg="white", width = 0, height = 0, command=p7.lift)
 
         b1.pack(side="left", padx=40)
         b2.pack(side="left", padx=40)
@@ -156,8 +164,18 @@ class MainView(tk.Frame):
         b4.pack(side="left", padx=40)
         b5.pack(side="left", padx=40)
         b6.pack(side="bottom")
+        #b7.pack(side="bottom")
+
+        menubar = tk.Menu(root)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Settings", command=p7.lift)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=root.quit)
+        menubar.add_cascade(label="Settings", menu=filemenu)
+        root.config(menu=menubar)
 
         p6.show()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -165,6 +183,7 @@ if __name__ == "__main__":
     main.pack(side="top", fill="both", expand=True)
     root.title("Crypto Tracker")
     root.wm_geometry("1280x720")
+#
     root.mainloop()
 
 
