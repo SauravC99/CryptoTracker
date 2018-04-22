f7db258b405a171a8873d03acb55526cef726944
Marjorie Balaoro <marjoriebalaoro16@gmail.com>
2018-04-21 17:19:06 -0700

    updated file

--- API.py
+++ API.py
@@ -1,3 +1,10 @@
-#this is the API python file
+#This is the API Python file
 
-#testing
+
+# # # # # LIMITS FOR COINMARKET CAP API # # # # #
+
+#   1. Limit requests to no more than 10 per minute #
+
+#   2. Endpoints update every 5 minutes             #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
