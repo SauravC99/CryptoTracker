13e6ae991f98a2c403c405cdda7b01f486594986
unknown <sauravc41@gmail.com>
2018-04-22 05:20:59 -0700

    Added SMS alert method and get information methods

--- percentChange.py
+++ percentChange.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+from telesign.messaging import MessagingClient
 from API import *
 import time
 
@@ -18,6 +20,27 @@ def percentChange(price):
     return change
 
 def alert(percent):
+    #Telesign API Code
+    customer_id = "BA0A590D-1B52-49E7-8C20-C082D986B309"
+    api_key = "5TIV0NDzn2afvWmWscxjPidghGWEY+55hZYXSig4dgML6uUZ67pWvcGa9JtswmS1hea8GMP7HQmBHnB1gwIasg=="
+
+    phone_number = getPhoneNumber()
+    message = "{0} has gone up {1} percent in the last 5 minutes!".format(getCoin(), percent)
+    message_type = "ARN"
+
+    messaging = MessagingClient(customer_id, api_key)
+    response = messaging.message(phone_number, message, message_type)
+
+def getThreshold():
+    #Get from graphics float
+    pass
+
+def getPhoneNumber():
+    #Get number from graphics int
+    pass
+
+def getCoin():
+    #Get from graphics String
     pass
 
 def main():
@@ -25,8 +48,14 @@ def main():
         #Get dictionary from API
         all_coins = getPrice()
         #Replace with coin from graphics
-        coin = all_coins["BTC"]
+        coin = all_coins[getCoin()]
+
+        #Get percent change
         value = percentChange(coin)
+        #Send SMS alert if it is greater than user's threshold
+        if value >= getThreshold():
+            alert(value)
+
         print(value)
         #Loop every 5 minutes because that's when API updates
         time.sleep(300)
