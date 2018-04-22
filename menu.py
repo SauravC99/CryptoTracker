eaeeaf657ac42621f8df20e400b9ef34bef0daab
Nathan Gardner <brytstahr@yahoo.com>
2018-04-21 20:51:43 -0700

    changed button to be held on the frame instead of the entire page

--- menu.py
+++ menu.py
@@ -1,23 +1,28 @@
 from tkinter import *
-from graph import *
+import CryptoTracker.graph as graph
 
 dict = {0 : 1,
         1 : 4,
         2 : 9,
-        3 : 16}
+        3 : 16,
+        4 : 25,
+        5 : 36,
+        6 : 49,
+        7 : 64}
 def gui():
     #initialize the menu
     menu = Tk()
     #create a frame for the menu - can be used to organize later
     frame = Frame(menu)
+    frame.pack()
     #not sure why bottomframe is necessary yet
     bottomframe = Frame(menu)
     bottomframe.pack(side = BOTTOM)
     #create resolution and title
-    menu.geometry('1920x1080')
+    menu.geometry('800x600')
     menu.title("Crypto Tracker")
     #create button that graphs dictionary on press
-    button = Button(menu, text="Graph", bg='black', fg='white', command= lambda: graph(dict))
+    button = Button(frame, text="Graph", bg='black', fg='white', command= lambda: graph.graph(dict))
     button.pack(side = LEFT)
     menu.mainloop()
 
