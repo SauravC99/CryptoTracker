0b7d087c62a2e9abe7e3687a8fd1b4437a0e3529
Nathan Gardner <brytstahr@yahoo.com>
2018-04-21 18:10:45 -0700

    added imports of modules needed

--- graph.py
+++ graph.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def graph(dict):
     #sort given dictionary into tuples and graph
     plt.plot(*zip(*sorted(dict.items())))
     plt.show()
-    
